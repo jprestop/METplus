05ab0c00c66643972811ef4f2ba820e8c6a4fbe5
Todd Arbetter <37349300+fentoad72@users.noreply.github.com>
2020-09-24 09:38:28 -0600

    moved docker load, docker build, docker save into docker_setup.sh and added timing

--- ci/travis_jobs/docker_setup.sh
+++ ci/travis_jobs/docker_setup.sh
@@ -5,6 +5,7 @@ echo 'doing docker build'
 
 #${TRAVIS_BUILD_DIR}/ci/travis_jobs/get_data_volumes.py
 
+### Loading Docker Image from cache
 echo 'Timing Docker Load'
 SECONDS=0
 IMAGE=docker_images/images.tar
@@ -18,11 +19,24 @@ duration=$SECONDS
 echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
 echo 
 
-echo Timing docker build...
+### Building new docker image hopefully with help from cache
+echo 'Timing docker Build'
 SECONDS=0
 
 docker build -t ${DOCKERHUB_TAG} --build-arg SOURCE_BRANCH=${DOCKERHUB_DEFAULT_TAGNAME} --build-arg MET_BRANCH=${DOCKERHUB_MET_TAGNAME} --build-arg DO_GIT_CLONE ${TRAVIS_BUILD_DIR}/ci/docker
 
 duration=$SECONDS
 echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
+
+### Save newly built image dtcenter/METplus:develop
+echo 'Timing Docker Save'
+SECONDS=0
+echo "Saving Docker Images"
+docker images
+# docker save -o docker_images/images.tar $(docker images -a -q)
+echo docker save -o docker_images/images.tar ${DOCKERHUB_TAG}
+docker save -o docker_images/images.tar ${DOCKERHUB_TAG}
+duration=$SECONDS
+echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
+
 echo 'done'
